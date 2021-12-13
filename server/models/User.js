961751251f63d93444394586d1979dd4ee482f3d
Misael Duarte <msl.snzn@gmail.com>
2021-12-13 15:41:35 +0000

    Agregado modelo de usuario

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
   {
@@ -12,4 +12,4 @@ const userSchema = mongoose.Schema(
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = User
+export default User
